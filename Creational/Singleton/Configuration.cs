551f32973641bae0e0f1959cdbfe9a015cf1bb1b
Jakub Kozera <kozerakuba@gmail.com>
2021-04-19 20:32:33 +0200

    Singleton

---
+++ Creational/Singleton/Configuration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Configuration
+    {
+        private static Configuration _instance = null;
+        private static object obj = new object();
+        public string StringProperty { get; set; }
+        public int IntProperty { get; set; }
+
+        private Configuration()
+        {
+        }
+
+        public static Configuration GetInstance()
+        {
+            lock (obj)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Configuration();
+                }
+            }
+
+
+            return _instance;
+        }
+
+    }
+}
--- Creational/Singleton/Program.cs
+++ Creational/Singleton/Program.cs
@@ -7,6 +7,14 @@ namespace Singleton
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var cfg = Configuration.GetInstance();
+            var cfg2 = Configuration.GetInstance();
+
+            if (ReferenceEquals(cfg, cfg2))
+            {
+                Console.WriteLine("Configuration is a singleton");
+            }
         }
     }
 }
